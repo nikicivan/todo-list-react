c7b3be5742a5492286db08fdcc1a209156c0afbe
unknown <ivan.nikic@gmail.com>
2020-09-06 03:50:44 +0200

    Updates

---
+++ precache-manifest.e71df3c78401a89a34f79ea1f69de48b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "278b78571ad7fe39377e566a91e1850b",
+    "url": "/todo-list-react/index.html"
+  },
+  {
+    "revision": "1708769ff10431657340",
+    "url": "/todo-list-react/static/css/main.69c889d6.chunk.css"
+  },
+  {
+    "revision": "670fd04fc78956c2fb88",
+    "url": "/todo-list-react/static/js/2.1ef4f97a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/todo-list-react/static/js/2.1ef4f97a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1708769ff10431657340",
+    "url": "/todo-list-react/static/js/main.a769008d.chunk.js"
+  },
+  {
+    "revision": "d2ce1cbae4c6bd42d20e",
+    "url": "/todo-list-react/static/js/runtime-main.dc5b2452.js"
+  }
+]);
